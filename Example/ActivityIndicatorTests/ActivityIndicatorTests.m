8f333f00141113bc5b49a58ad1088170b597b79c
Aleh Dzenisiuk <aleh@touchtribe.nl>
2015-04-13 12:42:36 +0200

    Added view-based test into the example.

--- Example/ActivityIndicatorTests/ActivityIndicatorTests.m
+++ Example/ActivityIndicatorTests/ActivityIndicatorTests.m
@@ -5,32 +5,78 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import <FBSnapshotTestCase/FBSnapshotTestCase.h>
+#import <TTWKActivityIndicator/TTWKActivityIndicator.h>
 
-@interface ActivityIndicatorTests : XCTestCase
+@interface ActivityIndicatorTests : FBSnapshotTestCase
 @end
 
 @implementation ActivityIndicatorTests
 
 - (void)setUp {
+
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+	self.recordMode = NO;
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
+- (UIView *)testViewForImage:(UIImage *)image {
+	UIImageView *v = [[UIImageView alloc] initWithImage:image];
+	v.contentMode = UIViewContentModeCenter;
+	[v sizeToFit];
+	return v;
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)checkImage:(UIImage *)image prefix:(NSString *)prefix {
+	NSError *error = nil;
+	BOOL ok = [self
+		compareSnapshotOfView:[self testViewForImage:image]
+		referenceImagesDirectory:[NSString stringWithFormat:@"%s", FB_REFERENCE_IMAGE_DIR]
+		identifier:prefix
+		error:&error
+	];
+	XCTAssertTrue(ok, @"Snapshot comparison failed: %@", error); \
+}
+
+- (void)checkIndicator:(TTWKActivityIndicator *)indicator prefix:(NSString *)prefix {
+
+	NSArray *images = [[indicator animatedImage] images];
+
+	[self checkImage:[images firstObject] prefix:[NSString stringWithFormat:@"%@-first", prefix]];
+	[self checkImage:[images objectAtIndex:images.count / 4] prefix:[NSString stringWithFormat:@"%@-middle", prefix]];
+	[self checkImage:[images lastObject] prefix:[NSString stringWithFormat:@"%@-last", prefix]];
+}
+
+- (void)testModernDefault {
+	TTWKActivityIndicator *indicator = [[TTWKActivityIndicator alloc] initWithColor:[UIColor colorWithRed:0.1216 green:0.5412 blue:0.4392 alpha:1.0]];
+	[self checkIndicator:indicator prefix:@"modern-default"];
+}
+
+- (void)testModernSmall {
+	TTWKActivityIndicator *indicator = [[TTWKActivityIndicator alloc] initWithColor:[UIColor colorWithRed:0.7490 green:0.8510 blue:0.2824 alpha:1.0]];
+	[self checkIndicator:indicator prefix:@"modern-small"];
+}
+
+- (void)testBetaDefault {
+	TTWKActivityIndicator *indicator = [[TTWKActivityIndicator alloc]
+		initWithStyle:TTWKActivityIndicatorStyleBeta
+		color:[UIColor colorWithRed:0.9922 green:0.4549 blue:0.0000 alpha:1.0]
+		bubbleRadius:0
+	];
+	[self checkIndicator:indicator prefix:@"beta-default"];
+}
+
+- (void)testBetaSmall {
+	TTWKActivityIndicator *indicator = [[TTWKActivityIndicator alloc]
+		initWithStyle:TTWKActivityIndicatorStyleBeta
+		color:[UIColor colorWithRed:1.0000 green:0.8824 blue:0.1020 alpha:1.0]
+		bubbleRadius:4
+	];
+	[self checkIndicator:indicator prefix:@"beta-small"];
 }
 
 @end
